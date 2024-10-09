d0c87678c9ea8e0bd7cd4c8029c2cd990ec84114
dsmundhe <dipakmundhe2026@gmail.com>
2024-10-09 12:04:45 -0700

    perfect square

---
+++ LinearSearch/PerfectSquareRoot.java
@@ -0,0 +1,20 @@
+public class PerfectSquareRoot {
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 15, 26, 28, 25, 36, 68, 95, 63, 56 };
+        int result = findPerfectSquare(arr);
+        System.out.println("Perfect Square : " + result);
+    }
+
+    static int findPerfectSquare(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int squrt = (int) Math.pow(arr[i], 0.5f);
+            for (int j = 0; j < arr.length; j++) {
+                if (squrt * squrt == arr[j]) {
+                    return arr[j];
+                }
+            }
+        }
+        return -1;
+    }
+}
