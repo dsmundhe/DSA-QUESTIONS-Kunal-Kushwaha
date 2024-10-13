53ef840f281b5a516add7e0f38f1998c2dd65e73
dsmundhe <dipakmundhe2026@gmail.com>
2024-10-13 04:15:13 -0700

    selection sort

---
+++ Sorting/Selection Sort/SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 100, 3, 2, 1 };
+        selSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] selSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int maxIndex = 0;
+            int maxElement = arr[0];
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] >= maxElement) {
+                    maxIndex = j;
+                }
+            }
+            int temp = arr[maxIndex];
+            arr[maxIndex] = arr[(arr.length - i) - 1];
+            arr[(arr.length - i) - 1] = temp;
+        }
+        return arr;
+    }
+
+}
