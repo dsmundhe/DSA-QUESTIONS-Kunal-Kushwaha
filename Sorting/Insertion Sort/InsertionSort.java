5c71dcd5f3dca5d885702bc19977a82363da70bf
dsmundhe <dipakmundhe2026@gmail.com>
2024-10-13 09:43:17 -0700

    insertion sort

---
+++ Sorting/Insertion Sort/InsertionSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1, 0 };
+        inSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] inSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j - 1, j);
+                }
+            }
+        }
+        return arr;
+    }
+
+    static int[] swap(int[] arr, int num1, int num2) {
+        int temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        return arr;
+    }
+}
