a758e57b191ac885eda6839f785ad9bcb4a9d048
dsmundhe <dipakmundhe2026@gmail.com>
2024-11-14 21:15:19 -0800

    cycle sort practice

--- Practice/CycleSort.java
+++ Practice/CycleSort.java
@@ -3,21 +3,23 @@ import java.util.Arrays;
 public class CycleSort {
     public static void main(String[] args) {
         int[] arr = { 8, 7, 6, 5, 4, 3, 2, 1 };
-        cySort(arr);
+        System.out.println(Arrays.toString(arr));
+        cySortFun(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    static void cySort(int[] arr) {
+    static void cySortFun(int[] arr) {
         int i = 0;
         while (i < arr.length) {
-            int correct = arr[i] - 1;
-            if (arr[i] != arr[correct]) {
+            int orgPosition = arr[i] - 1;
+            if (arr[i] != arr[orgPosition]) {
                 int temp = arr[i];
-                arr[i] = arr[correct];
-                arr[correct] = temp;
+                arr[i] = arr[orgPosition];
+                arr[orgPosition] = temp;
             } else {
                 i++;
             }
         }
+
     }
 }
