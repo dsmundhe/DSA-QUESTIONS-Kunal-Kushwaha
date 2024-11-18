91290f2a3ed923480f303abcdb3bb17791b8c475
dsmundhe <dipakmundhe2026@gmail.com>
2024-11-18 10:07:49 -0800

    recursion use

--- Recursion/Recursion01/Recursion.java
+++ Recursion/Recursion01/Recursion.java
@@ -1,5 +1,24 @@
 
-
 public class Recursion {
-    
+    public static void main(String[] args) {
+        // print nums 1 to 10 using recursion
+        nums(1);
+        messages(1);
+    }
+
+    static void nums(int n) {
+        if (n == 5) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        nums(n + 1);
+    }
+
+    static void messages(int num) {
+        if (num == 6)
+            return;
+        System.out.println("Hello World");
+        messages(num + 1);
+    }
 }
