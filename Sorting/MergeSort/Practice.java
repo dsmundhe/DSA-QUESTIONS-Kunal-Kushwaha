24159d434237fa109a582fd0d69efd9894fa25df
dsmundhe <dipakmundhe2026@gmail.com>
2025-01-17 19:42:20 -0800

    merger sort practice

--- Sorting/MergeSort/Practice.java
+++ Sorting/MergeSort/Practice.java
@@ -1,51 +1,52 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
-class Practice {
-    public static void main(String[] args) {
-        int[] arr = { 5, 8, 7, 9, 10, 12, 3, 4, 14 };
-        System.out.println(Arrays.toString(mergerSort(arr)));
+public class Practice {
+
+    public static void main(String args[]) {
+        int[] arr = { 8, 7, 9, 4, 5, 1, 6, 3, 2 };
+        System.out.print("Before : ");
+        System.out.println(Arrays.toString(arr));
+        System.out.print("After : ");
+        System.out.println(Arrays.toString(mergeSort(arr)));
     }
 
-    static int[] mergerSort(int[] arr) {
+    static int[] mergeSort(int[] arr) {
         if (arr.length == 1) {
             return arr;
         }
         int mid = arr.length / 2;
+        int[] leftArr = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] rightArr = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
 
-        int[] leftArr = mergerSort(Arrays.copyOfRange(arr, 0, mid));
-        int[] rightArr = mergerSort(Arrays.copyOfRange(arr, mid, arr.length));
         return merge(leftArr, rightArr);
-
     }
 
-    static int[] merge(int[] leftArr, int[] rightArr) {
+    static int[] merge(int[] left, int[] right) {
         int i = 0;
         int j = 0;
         int k = 0;
-        int[] mixArr = new int[leftArr.length + rightArr.length];
-
-        while (i < leftArr.length && j < rightArr.length) {
-            if (leftArr[i] < rightArr[j]) {
-                mixArr[k] = leftArr[i];
+        int[] mix = new int[left.length + right.length];
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                mix[k] = left[i];
                 i++;
             } else {
-                mixArr[k] = rightArr[j];
+                mix[k] = right[j];
                 j++;
             }
             k++;
         }
-
-        while (i < leftArr.length) {
-            mixArr[k] = leftArr[i];
+        while (i < left.length) {
+            mix[k] = left[i];
             i++;
             k++;
         }
-        while (j < rightArr.length) {
-            mixArr[k] = rightArr[j];
+        while (j < right.length) {
+            mix[k] = right[j];
             j++;
             k++;
         }
-
-        return mixArr;
+        return mix;
     }
 }
