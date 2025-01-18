c92358e49501d9bf469017b7bb6f718b2aa40e9d
dsmundhe <dipakmundhe2026@gmail.com>
2025-01-17 19:48:24 -0800

    bubble sort practice

--- Sorting/Bubble Sort/Practice.java
+++ Sorting/Bubble Sort/Practice.java
@@ -2,12 +2,13 @@ import java.util.Arrays;
 
 public class Practice {
     public static void main(String[] args) {
-        int[] arr = { 5, 4, 3, 2, 1, 0 };
-        bubblSort(arr);
+        int[] arr = { 8, 7, 9, 4, 5, 1, 6, 3, 2 };
+        bubSort(arr);
         System.out.println(Arrays.toString(arr));
+
     }
 
-    static void bubblSort(int[] arr) {
+    static void bubSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j - 1]) {
