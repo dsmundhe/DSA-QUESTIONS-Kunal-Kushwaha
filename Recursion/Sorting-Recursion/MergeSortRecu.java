5425993d16ae9e404b7cd46ba7a1f5f71030055c
dsmundhe <dipakmundhe2026@gmail.com>
2025-01-28 18:53:14 -0800

    qickSort recursion

--- Recursion/Sorting-Recursion/MergeSortRecu.java
+++ Recursion/Sorting-Recursion/MergeSortRecu.java
@@ -5,6 +5,55 @@ public class MergeSortRecu {
         int[] arr = { 5, 3, 4, 2, 1, 0 };
         int[] ans = mergeSort(arr);
         System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.toString(arr));
+        mergeSortInplace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // Through Inplace Logic
+    static void mergeSortInplace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+        int mid = s + (e - s) / 2;
+        mergeSortInplace(arr, s, mid);
+        mergeSortInplace(arr, mid, e);
+
+        mergerInPlace(arr, s, mid, e);
+    }
+
+    // Merger inplace
+
+    static void mergerInPlace(int[] arr, int s, int mid, int e) {
+
+        int[] mixArr = new int[e - s];
+        int i = s;
+        int j = mid;
+        int k = 0;
+        while (i < mid && j < e) {
+            if (arr[i] < arr[j]) {
+                mixArr[k] = arr[i];
+                i++;
+            } else {
+                mixArr[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < mid) {
+            mixArr[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j < e) {
+            mixArr[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (int f = 0; f < mixArr.length; f++) {
+            arr[ f+s] = mixArr[f];
+        }
+      
     }
 
     static int[] mergeSort(int[] arr) {
@@ -18,17 +67,17 @@ public class MergeSortRecu {
         return merge(left, right);
     }
 
-    static int[] merge(int[] first, int[] second) {
-        int[] mixArr = new int[first.length + second.length];
+    static int[] merge(int[] first, int[] arr) {
+        int[] mixArr = new int[first.length + arr.length];
         int i = 0;
         int j = 0;
         int k = 0;
-        while (i < first.length && j < second.length) {
-            if (first[i] < second[j]) {
+        while (i < first.length && j < arr.length) {
+            if (first[i] < arr[j]) {
                 mixArr[k] = first[i];
                 i++;
             } else {
-                mixArr[k] = second[j];
+                mixArr[k] = arr[j];
                 j++;
             }
             k++;
@@ -38,8 +87,8 @@ public class MergeSortRecu {
             i++;
             k++;
         }
-        while (j < second.length) {
-            mixArr[k] = second[j];
+        while (j < arr.length) {
+            mixArr[k] = arr[j];
             j++;
             k++;
         }
