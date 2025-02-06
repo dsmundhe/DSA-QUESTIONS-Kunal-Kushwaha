fad736f2e6bc62cd0af5d203a17d7c4d0f198204
dsmundhe <dipakmundhe2026@gmail.com>
2025-02-05 18:59:29 -0800

    string functions

---
+++ String/FunctionsOfString.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class FunctionsOfString {
+    public static void main(String[] args) {
+        String st = new String("DIPAK");
+
+        // stirng to charArr
+        char[] str = st.toCharArray();
+        System.out.println(Arrays.toString(str));
+
+        // chararr to string
+        System.out.println(new String(str));
+
+        // sentence to array
+        String se = "Dipak Samadhan Munde";
+        char[] chArr = se.toCharArray();
+        System.out.println(Arrays.toString(chArr));
+
+        // split function
+        String[] stArr = se.split(" ");
+        System.out.println(Arrays.toString(stArr));
+
+        //reverse String
+        System.out.println(se.concat(" DM"));
+    }
+}
