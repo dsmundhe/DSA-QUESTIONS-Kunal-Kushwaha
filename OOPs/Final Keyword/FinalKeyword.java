55ffca12a91233ef4a01d87d9709f1245e706974
dsmundhe <dipakmundhe2026@gmail.com>
2025-02-25 21:51:18 -0800

    final keyword

---
+++ OOPs/Final Keyword/FinalKeyword.java
@@ -0,0 +1,19 @@
+public class FinalKeyword {
+    public static void main(String[] args) {
+
+        final int a = 50;
+
+        // those variables are assign with final keyword those can not be reassign
+        // a=20;
+
+        System.out.println(a);
+        fun(a);
+        System.out.println(a);
+    }
+
+    final static void fun(int a) {
+        // this is happing because in java there is pass by value concept ;
+        a = a * 5;
+        System.out.println("value : " + (++a));
+    }
+}
---
+++ OOPs/WrapperClasses/WrapperClass.java
@@ -0,0 +1,13 @@
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        Integer num = 50;
+        System.out.println(num);
+        System.out.println(num.intValue());
+        System.out.println(num.floatValue());
+        System.out.println((int) num);
+        System.out.println(num.byteValue());
+        System.out.println(num.describeConstable());
+
+    }
+}
