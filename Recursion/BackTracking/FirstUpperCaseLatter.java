89b169dca94ba13946497f33c0853e33f427a38e
dsmundhe <dipakmundhe2026@gmail.com>
2025-03-06 05:36:15 -0800

    first uppercase letter in string

---
+++ Recursion/BackTracking/FirstUpperCaseLatter.java
@@ -0,0 +1,22 @@
+  
+class FirstUpperCaseLatter {
+    
+public  static  void  main(String[] args){
+    String str="dipAk";
+    System.out.println(firstUpperCase(str,0));
+    int a=10;
+    Integer b = new Integer(20);
+}
+public static char firstUpperCase(String str,int n){
+    if(n==str.length()){
+        return '-';
+    }
+    char ch=str.charAt(n);
+    if(ch >= 'A' && ch <='Z'){
+        return ch;
+    }else{
+        return firstUpperCase(str,n+1);
+    }
+}
+    
+}
